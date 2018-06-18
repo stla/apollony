a1ee58678d512a3d46b2254e86fa801975d05e31
stla <laurent_step@yahoo.fr>
2018-06-18 13:30:34 +0200

    use soddyCircle function

--- src/Apollony/Apollony1.hs
+++ src/Apollony/Apollony1.hs
@@ -1,10 +1,10 @@
 module Apollony.Apollony1
   where
-import Soddy.SoddyCircle (Point2, soddyRadius, soddyCenter)
+import Soddy.SoddyCircle (Point2, soddyCircle)
 
 apollony :: (Point2, Double) -> (Point2, Double) -> (Point2, Double) -> Int
          -> [(Point2, Double)]
-apollony c1@(p1,r1) c2@(p2,r2) c3@(p3,r3) n =
+apollony c1 c2 c3 n =
   if n==1
     then [circle]
   else
@@ -12,17 +12,7 @@ apollony c1@(p1,r1) c2@(p2,r2) c3@(p3,r3) n =
            , apollony c1 circle c3 (n-1)
            , apollony circle c2 c3 (n-1) ]
   where
-  circle = (soddyCenter p1 p2 p3, soddyRadius r1 r2 r3)
-
--- apollony :: (Point2, Double) -> (Point2, Double) -> (Point2, Double) -> Int
---          -> [(Point2, Double)]
--- apollony c1@(p1,r1) c2@(p2,r2) c3@(p3,r3) n =
---   if n==1
---     then [(soddyCenter p1 p2 p3, soddyRadius r1 r2 r3)]
---   else
---     concat [ apollony c1 c2 (soddyCenter p1 p2 p3, soddyRadius r1 r2 r3) (n-1)
---            , apollony c1 (soddyCenter p1 p2 p3, soddyRadius r1 r2 r3) c3 (n-1)
---            , apollony (soddyCenter p1 p2 p3, soddyRadius r1 r2 r3) c2 c3 (n-1) ]
+  circle = soddyCircle c1 c2 c3
 
 fractal :: Int -> [(Point2, Double)]
 fractal n = concatMap (apollony c1 c2 c3) [1..n]
